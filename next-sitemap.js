e6ccdb5d33ff571bad1aa101e804dd54649f5114
mendes <omatheusmendes@gmail.com>
2021-11-02 22:39:04 -0300

    FEA: sitemap and robots

---
+++ next-sitemap.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteUrl: 'https://carmelindaaraujo.com',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+};
