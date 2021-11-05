930e56981cfd577886129626a0ffa9dba9a57905
mendes <omatheusmendes@gmail.com>
2021-11-05 14:09:05 -0300

    FEA: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,13 @@ import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import 'styles/main.scss';
 import nextI18nextConfig from 'next-i18next.config';
+import ReactGA from 'react-ga';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    ReactGA.initialize('UA-212173071-1');
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
